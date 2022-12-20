f6fb10774bca184cda7558aba5cf145673116806
aldih suhandi <aldihsuhandi697@gmail.com>
2022-12-21 05:50:23 +0700

    init springboot

---
+++ src/main/java/id/thesis/shumishumi/ShumishumiApplication.java
@@ -0,0 +1,13 @@
+package id.thesis.shumishumi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShumishumiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShumishumiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/thesis/shumishumi/ShumishumiApplicationTests.java
@@ -0,0 +1,13 @@
+package id.thesis.shumishumi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShumishumiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
