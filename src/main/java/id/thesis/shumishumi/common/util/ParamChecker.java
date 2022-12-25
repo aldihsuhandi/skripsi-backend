796b7581ac31e1098c6c65016b5f85d3a8245ec1
aldih suhandi <aldihsuhandi697@gmail.com>
2022-12-25 21:01:46 +0700

    user api progress

---
+++ src/main/java/id/thesis/shumishumi/common/util/ParamChecker.java
@@ -0,0 +1,34 @@
+package id.thesis.shumishumi.common.util;
+
+import id.thesis.shumishumi.common.exception.ShumishumiException;
+import id.thesis.shumishumi.common.model.enumeration.ShumishumiErrorCodeEnum;
+
+public class ParamChecker {
+    public static void isNotNull(Object value, String param, ShumishumiErrorCodeEnum errorCode) throws ShumishumiException {
+        if (value == null) {
+            throwsException(String.format("%s cannot be null", param), errorCode);
+        }
+    }
+
+    public static void isNotEmpty(String value, String param, ShumishumiErrorCodeEnum errorCode) throws ShumishumiException {
+        if (value.isEmpty()) {
+            throwsException(String.format("%s cannot be empty", param), errorCode);
+        }
+    }
+
+    public static void isExpected(boolean value, String param, ShumishumiErrorCodeEnum errorCode) throws ShumishumiException {
+        if (!value) {
+            throwsException(String.format("the value of %s is not expected", param), errorCode);
+        }
+    }
+
+    public static void isExpected(String value, String comp, String param, ShumishumiErrorCodeEnum errorCode) throws ShumishumiException {
+        if (value.matches(comp)) {
+            throwsException(String.format("the value of %s is not expected", param), errorCode);
+        }
+    }
+
+    private static void throwsException(String msg, ShumishumiErrorCodeEnum errorCode) throws ShumishumiException {
+        throw new ShumishumiException(msg, errorCode);
+    }
+}
--- src/main/java/id/thesis/shumishumi/rest/request/user/UserRegisterRequest.java
+++ src/main/java/id/thesis/shumishumi/rest/request/user/UserRegisterRequest.java
@@ -1,6 +1,17 @@
 package id.thesis.shumishumi.rest.request.user;
 
 import id.thesis.shumishumi.rest.request.BaseRequest;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@ToString
 public class UserRegisterRequest extends BaseRequest {
+    private String email;
+    private String phoneNumber;
+    private String username;
+    private String password;
+    private String profilePicture;
 }
