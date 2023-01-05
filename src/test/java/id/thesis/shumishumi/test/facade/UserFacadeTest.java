7aa309e76842993af38d78664293c5cda9723a8f
aldih suhandi <aldihsuhandi697@gmail.com>
2023-01-05 19:10:08 +0700

    add unit test for user login

--- src/test/java/id/thesis/shumishumi/test/facade/UserFacadeTest.java
+++ src/test/java/id/thesis/shumishumi/test/facade/UserFacadeTest.java
@@ -3,12 +3,16 @@ package id.thesis.shumishumi.test.facade;
 import id.thesis.shumishumi.common.exception.ShumishumiException;
 import id.thesis.shumishumi.common.model.enumeration.ShumishumiErrorCodeEnum;
 import id.thesis.shumishumi.common.util.AssertUtil;
+import id.thesis.shumishumi.common.util.FunctionUtil;
 import id.thesis.shumishumi.core.facade.UserFacade;
 import id.thesis.shumishumi.dalgen.model.result.RoleDO;
 import id.thesis.shumishumi.dalgen.model.result.UserDO;
 import id.thesis.shumishumi.dalgen.service.RoleDAO;
+import id.thesis.shumishumi.dalgen.service.SessionDAO;
 import id.thesis.shumishumi.dalgen.service.UserDAO;
+import id.thesis.shumishumi.rest.request.user.UserLoginRequest;
 import id.thesis.shumishumi.rest.request.user.UserRegisterRequest;
+import id.thesis.shumishumi.rest.result.user.UserLoginResult;
 import id.thesis.shumishumi.rest.result.user.UserRegisterResult;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -28,6 +32,9 @@ public class UserFacadeTest {
 
     @MockBean
     private RoleDAO roleDAO;
+
+    @MockBean
+    private SessionDAO sessionDAO;
 
     @Test
     public void userRegisterTest_SUCCESS() {
@@ -50,14 +57,14 @@ public class UserFacadeTest {
     }
 
     @Test
-    public void userRegisterTest_Failed() {
+    public void userRegisterTest_FAILED() {
         UserRegisterRequest registerRequest = new UserRegisterRequest();
         registerRequest.setUsername("username");
         registerRequest.setEmail("email@email.com");
         registerRequest.setPassword("password");
         registerRequest.setPhoneNumber("12345");
 
-        Mockito.when(userDAO.queryByEmail(Mockito.any())).thenReturn(mockUserDO());
+        Mockito.when(userDAO.queryByEmail(Mockito.any())).thenReturn(mockUserDO("password"));
         Mockito.when(userDAO.queryByPhoneNumber(Mockito.any())).thenReturn(null);
         Mockito.when(roleDAO.queryById(Mockito.any())).thenReturn(mockRoleDO());
 
@@ -70,10 +77,68 @@ public class UserFacadeTest {
         }
     }
 
-    private UserDO mockUserDO() {
+    @Test
+    public void userLoginTest_SUCCESS() {
+        UserLoginRequest request = new UserLoginRequest();
+        String password = "password";
+        request.setEmail("email@email.com");
+        request.setPassword(password);
+
+        Mockito.when(userDAO.queryByEmail(Mockito.any())).thenReturn(mockUserDO(password));
+        Mockito.when(roleDAO.queryById(Mockito.any())).thenReturn(mockRoleDO());
+
+        UserLoginResult result = userFacade.login(request);
+        try {
+            AssertUtil.isExpected(result.getResultContext().isSuccess(),
+                    "result", ShumishumiErrorCodeEnum.SYSTEM_ERROR);
+        } catch (ShumishumiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void userLoginTest_FAILED_wrong_password() {
+        UserLoginRequest request = new UserLoginRequest();
+        String password = "password";
+        request.setEmail("email@email.com");
+        request.setPassword(password);
+
+        Mockito.when(userDAO.queryByEmail(Mockito.any())).thenReturn(mockUserDO("password2"));
+        Mockito.when(roleDAO.queryById(Mockito.any())).thenReturn(mockRoleDO());
+
+        UserLoginResult result = userFacade.login(request);
+        try {
+            AssertUtil.isExpected(!result.getResultContext().isSuccess(),
+                    "result", ShumishumiErrorCodeEnum.SYSTEM_ERROR);
+        } catch (ShumishumiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void userLoginTest_FAILED_user_not_exist() {
+        UserLoginRequest request = new UserLoginRequest();
+        String password = "password";
+        request.setEmail("email@email.com");
+        request.setPassword(password);
+
+        Mockito.when(userDAO.queryByEmail(Mockito.any())).thenReturn(null);
+
+        UserLoginResult result = userFacade.login(request);
+        try {
+            AssertUtil.isExpected(!result.getResultContext().isSuccess(),
+                    "result", ShumishumiErrorCodeEnum.SYSTEM_ERROR);
+        } catch (ShumishumiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private UserDO mockUserDO(String password) {
         UserDO user = new UserDO();
+        String hashPassword = FunctionUtil.hashPassword(password);
         user.setUserId("userId");
         user.setEmail("email");
+        user.setPassword(hashPassword);
 
         return user;
     }
