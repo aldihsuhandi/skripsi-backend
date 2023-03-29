50351c011c3b9229891c15f6100ff0bbb4cfd314
Aldih Suhandi <i-aldih.suhandi@dana.id>
2023-03-29 13:20:41 +0700

    add cors configuration

---
+++ src/main/java/id/thesis/shumishumi/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,31 @@
+package id.thesis.shumishumi.configuration;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Collections;
+
+@Configuration
+public class WebSecurityConfiguration {
+    @Bean
+    public CorsConfiguration corsConfiguration() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+
+        return config;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration());
+
+        return new CorsFilter(source);
+    }
+}
