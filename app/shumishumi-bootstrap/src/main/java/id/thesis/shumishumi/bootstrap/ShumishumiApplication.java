f42f06ff37f960d2f079faa8eba972e2f552defa
aldih suhandi <aldihsuhandi697@gmail.com>
2023-04-19 08:22:44 +0700

    finish migrating into bootstrap

--- app/shumishumi-bootstrap/src/main/java/id/thesis/shumishumi/bootstrap/ShumishumiApplication.java
+++ app/shumishumi-bootstrap/src/main/java/id/thesis/shumishumi/bootstrap/ShumishumiApplication.java
@@ -1,11 +1,13 @@
-package id.thesis.shumishumi;
+package id.thesis.shumishumi.bootstrap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+@ComponentScan("id.thesis.shumishumi")
 public class ShumishumiApplication {
 
     public static void main(String[] args) {
