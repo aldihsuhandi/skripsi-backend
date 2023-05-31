fc480f4d54c897896ed9addba3a1a1c40b9c4f4d
Aldih Suhandi <aldihsuhandi697@gmail.com>
2023-05-31 16:28:55 +0700

    add missing validator

---
+++ app/shumishumi-core/src/main/java/id/thesis/shumishumi/core/validator/review/ReviewQueryValidator.java
@@ -0,0 +1,20 @@
+package id.thesis.shumishumi.core.validator.review;
+
+import id.thesis.shumishumi.common.util.ParamChecker;
+import id.thesis.shumishumi.core.validator.BaseValidator;
+import id.thesis.shumishumi.facade.model.enumeration.ShumishumiErrorCodeEnum;
+import id.thesis.shumishumi.facade.request.BaseRequest;
+import id.thesis.shumishumi.facade.request.review.ReviewQueryRequest;
+import org.apache.commons.lang3.StringUtils;
+
+public class ReviewQueryValidator implements BaseValidator {
+    @Override
+    public void validate(BaseRequest baseRequest) {
+        ParamChecker.isNotNull(baseRequest, "request", ShumishumiErrorCodeEnum.PARAM_ILLEGAL);
+        ParamChecker.isExpected(baseRequest instanceof ReviewQueryRequest, "request", ShumishumiErrorCodeEnum.PARAM_ILLEGAL);
+
+        ReviewQueryRequest request = (ReviewQueryRequest) baseRequest;
+        ParamChecker.isExpected(StringUtils.equals(request.getType(), "MERCHANT") ||
+                StringUtils.equals(request.getType(), "USER"), "type", ShumishumiErrorCodeEnum.PARAM_ILLEGAL);
+    }
+}
