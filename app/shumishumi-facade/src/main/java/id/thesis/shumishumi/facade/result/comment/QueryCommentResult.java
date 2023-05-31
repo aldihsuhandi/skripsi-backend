1808a0f34848950f97a80f9846fab1b1508eddf1
Aldih Suhandi <aldihsuhandi697@gmail.com>
2023-05-31 23:59:50 +0700

    compose paging context into comment query result

--- app/shumishumi-facade/src/main/java/id/thesis/shumishumi/facade/result/comment/QueryCommentResult.java
+++ app/shumishumi-facade/src/main/java/id/thesis/shumishumi/facade/result/comment/QueryCommentResult.java
@@ -1,5 +1,6 @@
 package id.thesis.shumishumi.facade.result.comment;
 
+import id.thesis.shumishumi.facade.model.context.PagingContext;
 import id.thesis.shumishumi.facade.model.summary.CommentSummary;
 import id.thesis.shumishumi.facade.result.BaseResult;
 import lombok.Getter;
@@ -15,5 +16,5 @@ public class QueryCommentResult extends BaseResult {
     private static final long serialVersionUID = -220075206999215078L;
 
     private List<CommentSummary> comments;
-    private boolean hasNext;
+    private PagingContext pagingContext;
 }
