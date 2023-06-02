868b1a79d1a3fabcb46a56863b21a53b0b69e6f7
Aldih Suhandi <aldihsuhandi697@gmail.com>
2023-06-02 19:34:59 +0700

    change PARAM_ILLEGAL -> USER_INVALID on merchant apply

--- app/shumishumi-core/src/main/java/id/thesis/shumishumi/core/processor/user/MerchantApplyProcessor.java
+++ app/shumishumi-core/src/main/java/id/thesis/shumishumi/core/processor/user/MerchantApplyProcessor.java
@@ -35,7 +35,7 @@ public class MerchantApplyProcessor implements BaseProcessor {
         AssertUtil.isExpected(!StringUtils.equals(userVO.getRoleVO().getRoleName(),
                 UserRolesEnum.MERCHANT.getUserRoleName()), "user role is not expected", ShumishumiErrorCodeEnum.USER_ROLE_INVALID);
         AssertUtil.isExpected(!CollectionUtils.isEmpty(userVO.getLocation()),
-                "you need to fill out your address", ShumishumiErrorCodeEnum.PARAM_ILLEGAL);
+                "you need to fill out your address", ShumishumiErrorCodeEnum.USER_INVALID);
 
         RoleChangeInnerRequest innerRequest = new RoleChangeInnerRequest();
         innerRequest.setUserId(userId);
