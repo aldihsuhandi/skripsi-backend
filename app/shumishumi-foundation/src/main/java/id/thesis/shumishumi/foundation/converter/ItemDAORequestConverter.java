c516db81856bca777c5b16c214de2ad136ec6849
Aldih Suhandi <aldihsuhandi697@gmail.com>
2023-06-03 19:34:15 +0700

    fix itemId in create item

--- app/shumishumi-foundation/src/main/java/id/thesis/shumishumi/foundation/converter/ItemDAORequestConverter.java
+++ app/shumishumi-foundation/src/main/java/id/thesis/shumishumi/foundation/converter/ItemDAORequestConverter.java
@@ -5,7 +5,6 @@ package id.thesis.shumishumi.foundation.converter;
 
 
 import id.thesis.shumishumi.common.model.request.item.CreateItemInnerRequest;
-import id.thesis.shumishumi.common.util.FunctionUtil;
 import id.thesis.shumishumi.facade.model.constant.CommonConst;
 import id.thesis.shumishumi.facade.model.context.ItemFilterContext;
 import id.thesis.shumishumi.facade.model.context.ItemUpdateContext;
@@ -32,7 +31,7 @@ public class ItemDAORequestConverter {
         }
 
         ItemDO item = new ItemDO();
-        item.setItemId(FunctionUtil.generateUUID());
+        item.setItemId(request.getItemId());
         item.setItemName(request.getItemName());
         item.setItemImages(imageStr);
         item.setItemDescription(request.getItemDescription());
