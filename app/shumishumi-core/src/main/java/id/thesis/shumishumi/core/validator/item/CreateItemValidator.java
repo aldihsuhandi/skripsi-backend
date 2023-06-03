cd08fdf2f0104f335e8fd60f7a2d84ac8cceac30
Aldih Suhandi <aldihsuhandi697@gmail.com>
2023-06-03 21:05:06 +0700

    add validation for item create

--- app/shumishumi-core/src/main/java/id/thesis/shumishumi/core/validator/item/CreateItemValidator.java
+++ app/shumishumi-core/src/main/java/id/thesis/shumishumi/core/validator/item/CreateItemValidator.java
@@ -26,12 +26,18 @@ public class CreateItemValidator implements BaseValidator {
         ParamChecker.isNotEmpty(request.getItemName(), "Name", ShumishumiErrorCodeEnum.PARAM_ILLEGAL);
 
         ParamChecker.isNotNull(request.getItemPrice(), "Price", ShumishumiErrorCodeEnum.PARAM_ILLEGAL);
+        ParamChecker.isExpected(request.getItemPrice() > 100, "Price", ShumishumiErrorCodeEnum.PARAM_ILLEGAL);
 
         ParamChecker.isNotEmpty(request.getItemImages(), "images", ShumishumiErrorCodeEnum.PARAM_ILLEGAL);
 
         ParamChecker.isNotNull(request.getItemQuantity(), "Quantity", ShumishumiErrorCodeEnum.PARAM_ILLEGAL);
+        ParamChecker.isExpected(request.getItemQuantity() > 0, "quantity", ShumishumiErrorCodeEnum.PARAM_ILLEGAL);
 
-        ParamChecker.isNotNull(InterestLevelEnum.findByName(request.getMerchantInterestLevel()), "merchantInterestLevel", ShumishumiErrorCodeEnum.PARAM_ILLEGAL);
+        ParamChecker.isNotEmpty(request.getItemImages(), "item images", ShumishumiErrorCodeEnum.PARAM_ILLEGAL);
+        ParamChecker.isExpected(request.getItemImages().size() <= 10, "item images", ShumishumiErrorCodeEnum.PARAM_ILLEGAL);
+
+        ParamChecker.isNotNull(InterestLevelEnum.findByName(request.getMerchantInterestLevel()),
+                "merchantInterestLevel", ShumishumiErrorCodeEnum.PARAM_ILLEGAL);
 
         ParamChecker.isNotEmpty(request.getHobbyName(), "hobbyName", ShumishumiErrorCodeEnum.PARAM_ILLEGAL);
 
